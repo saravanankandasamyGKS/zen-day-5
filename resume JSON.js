9299d0a96636150eed6f2caf16e54de9aedc745c
saravanankandasamyGKS <147834306+saravanankandasamyGKS@users.noreply.github.com>
2023-10-27 09:57:19 +0530

    2.Create your own resume data in JSON format
    2.Create your own resume data in JSON format

---
+++ resume JSON.js
@@ -0,0 +1,30 @@
+const myResume = {
+  name: "Saravanan K",
+  age: 27,
+  place: "Chennai",
+  mail: "gksaravanan008@gmail.com",
+  "phone no": 9345462927,
+  qualification: [
+    {
+      level: "UG",
+      dep: "EEE",
+      institute: "Shreenivasa Engineering College",
+      percentage: 63,
+    },
+    {
+      level: "HSC",
+      dept: "CSE",
+      institute: "GOVT HR SEC SCHOOL, Somewhere in Dharmapuri",
+      percentage: 57,
+    },
+    {
+      level: "SSLC",
+      institute: "GOVT HR SEC SCHOOL, Somewhere in Dharmapuri",
+      percentage: 70,
+    },
+  ],
+  languagesknown: ["Tamil", "English"],
+  programminglanguages: ["JavaScript"],
+};
+
+console.log(myResume);
